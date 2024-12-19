2ee0924670764bf3080e7ed1b104923f8ce2d39a
gaksss <clement.carlier2@gmail.com>
2024-12-19 13:43:39 +0100

    DONE: connexion et inscription Back

--- frontend/pages/inscription.php
+++ frontend/pages/inscription.php
@@ -15,17 +15,21 @@
     <main class="flex justify-center flex-col items-center">
         <h2 class="text-white text-4xl mt-10 md:text-6xl md:mt-24 font-bold">S'enregistrer</h2>
         <section class="flex justify-center items-center mt-10 md:mt-24 text-white w-[50%]" id="inscription">
-            <form class="flex gap-4 flex-col text-xl" action="" method="post">
+
+            <form class="flex gap-4 flex-col text-xl" action="../../backend/process/connections/userInscription.php" method="post">
                 <div class="md:grid grid-cols-2 grid-rows-2 items-center gap-x-52 gap-y-12">
 
                     <div class="flex flex-col ">
                         <label for="email">Votre email: </label>
-                        <input class="text-white bg-[#777777] rounded-lg py-2" type="mail" name="email" id="email">
+                        <input class="text-white bg-[#777777] rounded-lg py-2" type="text" name="email" 
+                        id="email">
                     </div>
+
                     <div class="flex flex-col ">
                         <label for="confirmEmail">Confirmer email: </label>
-                        <input class="text-white bg-[#777777] rounded-lg py-2" type="mail" name="confirmEmail" id="confirmEmail">
+                        <input class="text-white bg-[#777777] rounded-lg py-2 y-" type="text" name="confirmEmail" id="confirmEmail">
                     </div>
+
                     <div class="flex flex-col ">
                         <label for="password">Votre mot de passe: </label>
                         <input class="text-white bg-[#777777] rounded-lg py-2" type="password" name="password" id="password">
@@ -35,6 +39,7 @@
                         <label for="confirmPassword">Confirmer mot de passe: </label>
                         <input class="text-white bg-[#777777] rounded-lg py-2" type="password" name="confirmPassword" id="confirmPassword">
                     </div>
+
                 </div>
 
                 <label for="username">Votre nom d'utilisateur: </label>
@@ -43,11 +48,30 @@
                 <input class="bg-emerald-green text-3xl rounded-full px-4 py-4 mt-12 cursor-pointer text-white hover:bg-dark-blue" type="submit" value="Créer un compte">
 
             </form>
+            <?php if (isset($_GET['err']) && $_GET['err'] == 1) {
+                echo '<p class="absolute x-0 top-6 bg-red-700">Merci de ne pas supprimer les champs</p>';
+            }
 
+
+            if (isset($_GET['err']) && $_GET['err'] == 2) {
+                echo '<p class="absolute x-0 top-6 bg-red-700">Merci de remplir tout les champs</p>';
+            }
+            if (isset($_GET['err']) && $_GET['err'] == 3) {
+                echo '<p class="absolute x-0 top-6 bg-red-700">La longueur max des champs est de 50 caractères</p>';
+            }
+            if (isset($_GET['err']) && $_GET['err'] == 4) {
+                echo '<p class="absolute x-0 top-6 bg-red-700">Vérifier que les mots de passe soient les mêmes et que les emails soient les mêmes</p>';
+            }
+            if (isset($_GET['err']) && $_GET['err'] == 5) {
+                echo '<p class="absolute x-0 top-6 bg-red-700">Mail non valide</p>';
+            }
+
+            ?>
         </section>
     </main>
     <div id="stars">
     </div>
+
 </body>
 
 </html>
